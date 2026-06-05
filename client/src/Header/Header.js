847f2679db577bf49025a23fbb2804f829939043
gabrielnatividaddev-coder <gabriel.natividad.dev@gmail.com>
2026-06-05 10:21:52 +0800

    edits

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
           <Drawer
             opened={drawerOpened}
             onClose={closeDrawer}
-            size="50%"
+            size="60%"
             padding="md"
             title="Navigation"
             hiddenFrom="sm"
